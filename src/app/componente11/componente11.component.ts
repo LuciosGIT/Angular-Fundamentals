a62be4f66c762cf016b2de430377bb5b2f4cb6f7
LuciosGIT <jloliveirajunior1903@gmail.com>
2025-01-15 22:54:12 -0300

    Implementing delete feature

--- src/app/componente11/componente11.component.ts
+++ src/app/componente11/componente11.component.ts
@@ -75,4 +75,18 @@ export class Componente11Component {
       this.btnCadastrar=true;
     }
 
+    // Função de remoção
+    remover() { 
+      // Removendo pessoa do vetor
+      this.vetor.splice(this.indice, 1);
+
+      // Limpeza dos inputs
+      this.formulario.reset();
+
+      // Visibilidade dos botões
+      this.btnCadastrar=true;
+    }
+
+
+    
 }
